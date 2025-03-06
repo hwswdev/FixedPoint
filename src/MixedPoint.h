d72e8d1a35873f420850f806b44534a3c9614230
Evgeny Sobolev <hwswdevsev@gmail.com>
2025-03-07 02:09:57 +0300

    Mixed point thinking all before this is trash.
    So. I tried to think of fixed point shift.
    I used different MixedPoint class to shift fixed point on different operation automatically, depend on target class.

---
+++ src/MixedPoint.h
@@ -0,0 +1,181 @@
+/*
+ *  Created on: Mar 5, 2025
+ *      Author: Evgeny Sobolev
+ */
+
+#pragma once
+
+#include <stdint.h>
+#include <stddef.h>
+#include <math.h>
+
+
+template< typename BaseType, typename HelperType = BaseType, size_t DecimalSymAfterDot = 2, size_t FractionalBitCount = 4, size_t SymAfterDot = 0, typename... Limits >
+struct MixedPoint {
+	constexpr static const size_t FractionalPartDecimalSymbolCount = DecimalSymAfterDot;
+	constexpr static const size_t FractionalPartBinaryBitCount = FractionalBitCount;
+	constexpr static const BaseType DecimalMyltiplyer = pow( 10, FractionalPartDecimalSymbolCount );
+	constexpr static const BaseType BinaryMultiplyer  = pow( 2, FractionalPartBinaryBitCount );
+	constexpr static const BaseType ConversionMultiplyer = DecimalMyltiplyer * BinaryMultiplyer;
+
+	constexpr MixedPoint() : _value( 0 ) {};
+	constexpr MixedPoint( int value ) : _value( value * ConversionMultiplyer ) {};
+	constexpr MixedPoint( float value )  : _value( value * ConversionMultiplyer ) {};
+	constexpr MixedPoint( double value ) : _value( value * ConversionMultiplyer ) {};
+
+
+	constexpr inline float toFloat() const {
+		const float value = static_cast<float>( _value ) / ConversionMultiplyer;
+		return value;
+	}
+
+	constexpr inline double toDouble() const {
+		const double value = static_cast<double>( _value ) / ConversionMultiplyer;
+		return value;
+	}
+
+	template< typename OtherMixedPoint >
+	constexpr MixedPoint( const OtherMixedPoint& other ) {
+		_value = convert(other);
+	}
+
+	template< typename OtherMixedPoint >
+	constexpr inline const MixedPoint& operator+( const OtherMixedPoint& other ) const {
+		MixedPoint result;
+		result._value = _value + convert( other );
+
+		return result;
+	}
+
+	template< typename OtherMixedPoint >
+	constexpr inline const MixedPoint& operator-( const OtherMixedPoint& other ) const {
+		MixedPoint result;
+		result._value = _value - convert( other );
+
+		return result;
+	}
+
+	template< typename OtherMixedPoint >
+	constexpr inline const MixedPoint& operator*( const OtherMixedPoint& other ) const {
+		MixedPoint result;
+		HelperType temp = _value;
+		result._value = temp * convert( other ) / OtherMixedPoint::ConversionMultiplyer;
+		return result;
+	}
+
+	template< typename OtherMixedPoint >
+	constexpr inline const MixedPoint& operator/( const OtherMixedPoint& other ) const {
+		MixedPoint result;
+		HelperType temp = _value;
+		result._value = ( temp * OtherMixedPoint::ConversionMultiplyer) / convert( other );
+		return result;
+	}
+
+	template< typename OtherMixedPoint >
+	constexpr inline bool operator==( const OtherMixedPoint& other ) const {
+		return ( _value == convert( other ).nativeValue() );
+	}
+
+	template< typename OtherMixedPoint >
+	constexpr inline bool operator>( const OtherMixedPoint& other ) const {
+		return ( _value > convert( other ).nativeValue() );
+	}
+
+	template< typename OtherMixedPoint >
+	constexpr inline bool operator<( const OtherMixedPoint& other ) const {
+		return ( _value < convert( other ).nativeValue() );
+	}
+
+	template< typename OtherMixedPoint >
+	constexpr inline bool operator>=( const OtherMixedPoint& other ) const {
+		return ( _value >= convert( other ).nativeValue() );
+	}
+
+	template< typename OtherMixedPoint >
+	constexpr inline bool operator<=( const OtherMixedPoint& other ) const {
+		return ( _value <= convert( other ).nativeValue() );
+	}
+
+	constexpr inline bool operator+( const int value ) const {
+		// So, I think compile will shift it if it's power of 2
+		const BaseType temp = value * ConversionMultiplyer;
+		MixedPoint result;
+		result._value = _value + temp;
+		return result;
+	}
+
+	constexpr inline bool operator-( const int value ) const {
+		// So, I think compile will shift it if it's power of 2
+		const BaseType temp = value * ConversionMultiplyer;
+		MixedPoint result;
+		result._value = _value - temp;
+		return result;
+	}
+
+	constexpr inline bool operator*( const int value ) const {
+		MixedPoint result;
+		result._value = _value * value;
+		return result;
+	}
+
+	constexpr inline bool operator/( const int value ) const {
+		MixedPoint result;
+		result._value = _value / value;
+		return result;
+	}
+
+	constexpr inline BaseType nativeValue() const {
+		return _value;
+	}
+
+private:
+
+	template< typename OtherMixedPoint >
+	constexpr inline BaseType convert( const OtherMixedPoint& other ) const {
+		constexpr const size_t OtherDec = OtherMixedPoint::FractionalPartDecimalSymbolCount;
+		constexpr const size_t OtherBin = OtherMixedPoint::FractionalPartBinaryBitCount;
+		constexpr const size_t ThisDec = FractionalPartDecimalSymbolCount;
+		constexpr const size_t ThisBin = FractionalPartBinaryBitCount;
+		constexpr const bool TheSameDecScale = ( ThisDec == OtherDec );
+		constexpr const bool TheSameBinScale = ( ThisBin == OtherBin );
+		constexpr const bool TheSameScale = ( TheSameDecScale && TheSameBinScale );
+
+		if ( TheSameScale ) {
+			return  other.nativeValue();
+		}
+
+		if ( TheSameDecScale ) {
+			if ( ThisBin > OtherBin ) {
+				constexpr const size_t ShiftSize = ThisBin - OtherBin;
+				const BaseType result =  ( other.nativeValue() ) << ShiftSize;
+				return result;
+			} else {
+				constexpr const size_t ShiftSize = OtherBin - ThisBin;
+				// So I wanted to use shift (>>), but what if negative value
+				// I think compiler is not so stupid, and will make shift if possible
+				const BaseType DivValue =  ( static_cast<const BaseType>(1) << ShiftSize );
+				const BaseType result = ( other.nativeValue() ) / DivValue;
+				return result;
+			}
+		} else {
+			constexpr const BaseType ThisMultiplyer  = pow( 10, ThisDec )  * pow(2, ThisBin );
+			constexpr const BaseType OtherMultiplyer = pow( 10, OtherDec ) * pow(2, OtherBin );
+			if ( ThisMultiplyer >= OtherMultiplyer ) {
+				constexpr const BaseType MulValue = ThisMultiplyer / OtherMultiplyer;
+				const BaseType result = other.nativeValue() * MulValue;
+				return result;
+			} else {
+				constexpr const BaseType DivValue = OtherMultiplyer / ThisMultiplyer;
+				const BaseType result = other.nativeValue() / DivValue;
+				return result;
+			}
+		}
+
+		return 0;
+	}
+
+private:
+	BaseType _value {};
+};
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -8,35 +8,70 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#include "FixedPoint.h"
+#include "MixedPoint.h"
+#include "DecimalPoint.h"
 #include "DataDump.h"
 #include "Axis.h"
 
 
-typedef int32_t FixedPointBaseType;
-static constexpr const size_t FixedPointFracBitCount = 10;
-typedef FixedPoint<FixedPointBaseType, FixedPointFracBitCount> Fixed;
+typedef MixedPoint<int32_t, int32_t, 2, 4> MixedT1;
+typedef MixedPoint<int32_t, int32_t, 2, 8> MixedT2;
+typedef MixedPoint<int32_t, int32_t, 2, 2> MixedT3;
+typedef MixedPoint<int32_t, int32_t, 0, 4> MixedT4;
+typedef MixedPoint<int32_t, int32_t, 0, 2> MixedT5;
+typedef MixedPoint<int32_t, int32_t, 3, 6> MixedT6;
+typedef MixedPoint<int32_t, int32_t, 0, 6> MixedT7;
+
 
 static constexpr const size_t BufferSize = 1024;
 char buffer[BufferSize];
 
 void test() {
 
+	MixedT1 a;
+	MixedT2 b;
+	MixedT3 c;
+	MixedT4 d;
+	MixedT5 e;
+	MixedT6 f;
+	MixedT7 g;
 
-	const size_t sz = sizeof( Fixed );
+	a = 10.625;
+	b = a;
+	c = a;
+	d = a;
+	e = a;
+	f = a;
+	g = a;
+
+
+	size_t offset = 0;
+	offset += sprintf( buffer + offset, "a = %f, b = %f, c = %f, d = %f, e = %f, f= %f, g = %f\n ", a.toFloat(), b.toFloat(), c.toFloat(), d.toFloat(), e.toFloat(), f.toFloat(), g.toFloat() );
+	a = e;
+	f = e;
+	offset += sprintf( buffer + offset, "a = %f, b = %f, c = %f, d = %f, e = %f, f= %f, g = %f\n ", a.toFloat(), b.toFloat(), c.toFloat(), d.toFloat(), e.toFloat(), f.toFloat(), g.toFloat() );
+	asm("bkpt");
+
+#if 0
+	const Fixed   mmPerCircleXfix   = 5.00;
+	const Decimal mmPerCircleXdec   = 5.00;
+
+	const Fixed   stepsPerCircleFix = 200;
+	const Decimal stepsPerCircleDec = 200;
+
+	Fixed   stepSizeMmFix = mmPerCircleXfix / stepsPerCircleFix;
+	Decimal stepSizeMmDec = mmPerCircleXdec / stepsPerCircleDec;
 
 	asm("bkpt");
-	const Fixed mmPerCircleX = 4.990;
-	const Fixed stepsPerCircle = 200;
-	Fixed stepSizeMm = mmPerCircleX / stepsPerCircle;
-	asm("bkpt");
-	stepSizeMm = stepSizeMm;
-	asm("bkpt");
-	float stepMm = stepSizeMm.toFloat(6);
-	asm("bkpt");
-	sprintf( buffer, "value = %f, sz = %d", stepMm, sz );
+	float stepMmFix = stepSizeMmFix.toFloat();
+	float stepMmDec = stepSizeMmDec.toFloat();
+
 	asm("bkpt");
 
+	sprintf( buffer, "value = %f\n", stepMmFix );
+	sprintf( buffer, "value = %f\n", stepMmDec );
+	asm("bkpt");
+#endif
 }
 
 
