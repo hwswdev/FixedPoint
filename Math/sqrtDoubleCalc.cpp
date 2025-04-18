b1fd51b6ae4c26f8d17c9ef30f23c4a8462d2e31
Evgeny Sobolev <hwswdevsev@gmail.com>
2025-04-18 14:34:34 -0700

    Add files via upload
    Интересно. Сейчас при полном переборе значений 32-битного аргумента, максимальная ошибка составила -1.5675793156897271 
    Это модель, использующая 32-битные числа. Однако по ней видно, что следует стремиться оптипизировать коррекцию X.
    Я убрал табличку коррекции и посчитал коэффициенты прямо в коде модели.

---
+++ Math/sqrtDoubleCalc.cpp
@@ -0,0 +1,147 @@
+
+#include <math.h>
+#include <stdio.h>
+
+typedef unsigned char      uint8_t;
+typedef signed char        int8_t;
+typedef unsigned short     uint16_t;
+typedef signed short       int16_t;
+typedef unsigned int       uint32_t;
+typedef signed int         int32_t;
+typedef unsigned long long uint64_t;
+typedef signed long long   int64_t;
+
+
+uint8_t getMsb( const uint32_t val ) {
+	register uint32_t x = val;
+	register uint8_t bitCount = 32;
+	while( bitCount ) {
+		if ( x & 0x80000000 ) break;
+		x <<= 1;
+		bitCount--;
+	}
+	return bitCount;
+}
+
+static double gMaxLinearErr[32] = {0};
+
+double sqrtLinearApprox( const uint32_t val ) {
+	const uint8_t msb = getMsb(val) - 1;
+	const uint32_t X0 = static_cast<uint32_t>(1) << msb;
+	const uint32_t dX = (msb < 31) ? X0 : (X0 - 1); // So, correct because of maximum value is 0xFFFFFFFF
+	const double Y0 = sqrt( static_cast<double>(X0) );
+	const double dY = sqrt( static_cast<double>(X0 + dX) ) - Y0;
+	const double res = (val - X0) * dY / dX + Y0;
+	return res;
+}
+
+static double calcMaxLinearErr( const uint8_t msb ) {
+	static const double div = ( sqrt(2.0) - 1 ) * ( sqrt(2.0) - 1 );
+	static const double mul = 1 / div;
+	static const double sqrtTwo = sqrt(2.0);
+	static const double sqrtTwoMinusOne = sqrtTwo - 1.0;
+	double xMax = static_cast<double>( static_cast<uint32_t>(1) << (msb - 2) ) * mul;
+	const double dY = sqrt(xMax) - sqrtLinearApprox(static_cast<uint32_t>(xMax) );
+	return dY;	
+}
+
+void sqrtInit() {
+	for(uint32_t msb = 3; msb < 32; msb++) {
+		double dY = calcMaxLinearErr(msb);
+		gMaxLinearErr[msb] = dY;
+	}
+}
+
+
+double sqrtErrorApprox( const uint32_t val ) {
+	static const double div = ( sqrt(2.0) - 1 ) * ( sqrt(2.0) - 1 );
+	static const double mul = 1 / div;
+	const uint8_t msb = getMsb( val ) - 1;
+	const double xMid = (static_cast<uint32_t>(3) << (msb - 1));
+	const double xMax = static_cast<double>( static_cast<uint32_t>(1) << (msb - 2) ) * mul;
+	const double deltaX = xMid - xMax;
+	const double x = val;
+	const double a = x - xMid;
+	const double b = 1 << (msb - 1);
+	const double c = a / b;
+	const double d = c * c;
+	const double e = 1.0 - d;
+	const double xDiff = deltaX * e;
+	double corrX = a + xDiff;
+	const double f = corrX / ( static_cast<uint32_t>(1) << (msb - 1) );
+	const double g =  f * f;
+	const double h = 1.0 - g;
+	const double quadErr = gMaxLinearErr[msb] * h;
+	return quadErr;
+}
+
+
+double sqrtX( const uint32_t val ) {
+	
+	static bool initilized = false;
+
+	static const double sqrtTable[5] = {
+		0.0,				// sqrt(0)
+		1.0,				// sqrt(1)
+		1.41421356237309,	// sqrt(2)		
+		1.73205080756888,	// sqrt(3)
+		2.0 				// sqrt(4)
+	};
+
+	if (!initilized) {
+		sqrtInit();
+		initilized = true;
+	}
+
+	if (val <= 4) {
+		return sqrtTable[val];
+	}
+
+	const double linear = sqrtLinearApprox( val );
+	const double quad = sqrtErrorApprox( val );
+	const double res = linear + quad;
+	return res;
+}
+
+int main (int argc, char argv[]){
+
+	double maxPositiveError = 0.0;
+	double maxNegativeError = 0.0;
+
+	for(uint32_t x = 0; x < 65536; x++ ) {
+		const double valCalc = sqrtX(x);
+		const double valOrig = sqrt(static_cast<double>(x));
+		const double err = valCalc - valOrig;
+
+		if ( maxPositiveError < err ) {
+			maxPositiveError = err;
+		}
+
+		if ( maxNegativeError > err ) {
+			maxNegativeError = err;
+		}
+	}
+
+	for(uint32_t x = 0; x < static_cast<uint32_t>(0xFFFFFFF); x++ ) {
+		const double valCalc = sqrtX(x);
+		const double valOrig = sqrt(static_cast<double>(x));
+		const double err = valCalc - valOrig;
+
+
+		if ( maxPositiveError < err ) {
+			maxPositiveError = err;
+		}
+
+		if ( maxNegativeError > err ) {
+			maxNegativeError = err;
+		}
+
+	}
+	
+	// Max: 0.095480810223121715
+	// Min: -1.5675793156897271 ????? I don't know but I think it is error correction about 1LSB of X
+	printf("Max: %1.8f\n", maxPositiveError );
+	printf("Min: %1.8f\n", maxNegativeError );
+
+	return 0;
+}
